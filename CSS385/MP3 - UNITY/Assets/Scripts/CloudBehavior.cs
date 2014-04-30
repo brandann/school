7e7a725ed8ff9be80c116e14c7f0ec7c02724376
FungiSoftware <foilpudding@gmail.com>
2014-04-30 12:08:02 -0700

    added MP3, cleaned MP1, MP2
    fixed MP3 file system, removed extra files from MP1 and MP2 for project
    size management.

---
+++ CSS385/MP3 - UNITY/Assets/Scripts/CloudBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudBehavior : MonoBehaviour {
+
+	private float RX  =.05f;
+	private float RY  =.05f;
+	private float RR  =-1f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (RX, RY);
+		transform.Rotate(Vector3.forward, -1f * (RR * Time.smoothDeltaTime));
+	}
+}
