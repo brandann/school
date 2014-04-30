324e931153a20ec1b56dc1f69d2c590a494df12f
FungiSoftware <foilpudding@gmail.com>
2014-04-30 12:13:21 -0700

    added MP4
    fixed file system, cleaned up project folder.

---
+++ CSS385/MP4 - UNITY/Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileBehaviour : MonoBehaviour {
+
+	public float mSpeed = 100f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void Update () {
+		transform.position += (mSpeed * Time.smoothDeltaTime) * transform.up;
+		
+		GlobalBehavior globalBehavior = GameObject.Find ("GameManager").GetComponent<GlobalBehavior>();
+		GlobalBehavior.WorldBoundStatus status = globalBehavior.ObjectCollideWorldBound(this.transform.position, this.collider2D);
+		if (status != GlobalBehavior.WorldBoundStatus.Inside) {
+			Destroy(this.gameObject);
+			globalBehavior.EggCount--;
+		}
+	}
+	
+	public void SetForwardDirection(Vector3 f)
+	{
+		transform.up = f;
+	}
+	
+	
+}
